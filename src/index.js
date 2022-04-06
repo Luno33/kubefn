9a474b5ac9ba7eea2b3504573859db73ce14aca2
Marco Busacchi <marco.busacchi@sunrise.net>
2022-04-06 23:28:37 +0200

    Adds yargs

--- src/index.js
+++ src/index.js
@@ -1,80 +1,30 @@
 #!/usr/bin/env node
 
-const commandLineUsage = require('command-line-usage')
-const commandLineArgs = require('command-line-args')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const boostrap = require('./bootstrap')
 const compile = require('./compile')
 const run = require('./run')
 
-const optionDefinitions = [
-  { name: 'verbose', alias: 'v', type: Boolean },
-  { name: 'command', type: String, multiple: true, defaultOption: true },
-  { name: 'timeout', alias: 't', type: Number }
-]
-
-const options = commandLineArgs(optionDefinitions)
-
-const usage = commandLineUsage([
-  {
-    header: 'A typical app',
-    content: 'Generates something {italic very} important. This is a rather long, but ultimately inconsequential description intended solely to demonstrate description appearance. '
-  },
-  {
-    header: 'Options',
-    optionList: [
-      {
-        name: 'help',
-        description: 'Display this usage guide.',
-        alias: 'h',
-        type: Boolean
-      },
-      {
-        name: 'command',
-        description: 'The command to run',
-        type: String,
-        multiple: true,
-        defaultOption: true,
-        typeLabel: '{underline file} ...'
-      },
-      {
-        name: 'timeout',
-        description: 'Timeout value in ms.',
-        alias: 't',
-        type: Number,
-        typeLabel: '{underline ms}'
-      }
-    ]
-  },
-  {
-    content: 'Project home: {underline https://github.com/me/example}'
-  }
-])
-
-const basePath = ''
-const logicPath = ''
-
-async function bootstrap() {
-  console.log(options)
-  if (!('command' in options)) {
-    return;
-  }
-  if (options.command[0] === "bootstrap") {
-    if (options.command.length < 2) {
-      console.log('Specify a function name')
-      return;
-    }
-    boostrap(options.command[1])
-  }
-  if (options.command[0] === "compile") {
+yargs(hideBin(process.argv))
+  .usage('Usage: $0 <command> [options]')
+  .command('bootstrap <name>', 'create the function with name [name]', (yargs) => {
+    return yargs
+      .positional('name', {
+        describe: 'name of your function'
+      })
+  }, (argv) => {
+    console.info(`Init the project with name :${argv.name}`)
+    boostrap(argv.name)
+  })
+  .command('compile', 'copy your ./src folder in the blueprint under ./.kubefn', (yargs) => {
+    return yargs
+  }, (argv) => {
     compile()
-  }
-  if (options.command[0] === "run") {
+  })
+  .command('run', 'run locally your code in an expressjs server', (yargs) => {
+    return yargs
+  }, (argv) => {
     run()
-  }
-}
-
-bootstrap()
-
-
-
-
+  })
+  .parse()
