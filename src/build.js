2acee412557f61135be1edab1415166dd426a169
Marco Busacchi <marco.busacchi@sunrise.net>
2022-04-07 23:27:15 +0200

    Adds build command with correct npm version

--- src/build.js
+++ src/build.js
@@ -1,9 +1,21 @@
 const shell = require('shelljs');
 const compile = require('./compile')
+const fs = require('fs')
 
 module.exports = (semverIncrement) => {
   compile()
-  console.log(`Building the function...`)
   const currentDir = shell.pwd().stdout
-  shell.exec(`npm --prefix ${currentDir}/.kubefn/blueprint-fn-base/ run publish-${semverIncrement}`)
+
+  console.log('Incrementing the version')
+  const version = shell.exec(`npm version ${semverIncrement}`).stdout.replace('v', '').replace('\n', '')
+  // Here I'm using the output of npm version because by reading the package.json I get an outdated version
+
+  const packageJsonBlueprint = require(`${currentDir}/.kubefn/blueprint-fn-base/package.json`)
+
+  console.log(`Updating the new version (${version}) in the blueprint`)
+  packageJsonBlueprint.version = version
+  fs.writeFileSync(`${currentDir}/.kubefn/blueprint-fn-base/package.json`, JSON.stringify(packageJsonBlueprint, null, 2))
+
+  console.log(`Building the function ${packageJsonBlueprint.name}:${packageJsonBlueprint.version}...`)
+  shell.exec(`npm --prefix ${currentDir}/.kubefn/blueprint-fn-base/ run docker-all`)
 }
