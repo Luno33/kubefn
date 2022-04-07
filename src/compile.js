bb14420da17131bc6d13ae8ba37c6bbb5bc31a9d
Marco Busacchi <marco.busacchi@sunrise.net>
2022-04-07 23:04:15 +0200

    Overrides blueprint package json name

--- src/compile.js
+++ src/compile.js
@@ -9,21 +9,32 @@ module.exports = () => {
     return;
   }
 
+  if (!shell.test('-f', `${currentDir}/package.json`)) {
+    console.log('Cannot find the package.json file. Are you in the root folder of your project?')
+    return;
+  }
+
   console.log(`Compiling the function...`)
   shell.cp('-R', `${currentDir}/src`, `${currentDir}/.kubefn/blueprint-fn-base/`)
 
   console.log(`Load blueprint package.json...`)
-  const packageJson = require(`${currentDir}/.kubefn/blueprint-fn-base/package.json`)
+  const packageJsonBlueprint = require(`${currentDir}/.kubefn/blueprint-fn-base/package.json`)
 
   console.log(`Load kubefn.config.js config...`)
   const kubefnConfig = require(`${currentDir}/kubefn.config.js`)
 
+  console.log(`Load project package.json...`)
+  const packageJson = require(`${currentDir}/package.json`)
+
   console.log(`Override blueprint package.json configs with kubefn.config.js configs...`)
-  packageJson.config = {
-    ...packageJson.config,
+  packageJsonBlueprint.config = {
+    ...packageJsonBlueprint.config,
     ...kubefnConfig.config
   }
 
+  console.log(`Override project name...`)
+  packageJsonBlueprint.name = packageJson.name
+
   console.log(`Write new blueprint package.json...`)
-  fs.writeFileSync(`${currentDir}/.kubefn/blueprint-fn-base/package.json`, JSON.stringify(packageJson, null, 2))
+  fs.writeFileSync(`${currentDir}/.kubefn/blueprint-fn-base/package.json`, JSON.stringify(packageJsonBlueprint, null, 2))
 }
